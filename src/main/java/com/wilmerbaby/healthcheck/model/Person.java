c0e7e054356e59d2391e3ea4210f1b907e8b27bb
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-29 16:14:09 -0500

    started project with dependencies

---
+++ src/main/java/com/wilmerbaby/healthcheck/model/Person.java
@@ -0,0 +1,28 @@
+package com.wilmerbaby.healthcheck.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private long personId;
+    private String personFirstName;
+    private String personLastName;
+    private String personMiddleName;
+    private String maritalStatus;
+    private boolean legalName;
+    private String personLegalName;
+    private String formerName;
+    private String birthDay;
+    private int age;
+    private String sex;
+    private String personAddress;
+    private int ss_num;
+    private int home_phoneNumber;
+    private String cell_phoneNumber;
+    private String personEmail;
+    private String occupation;
+    private String employerName;
+    private int employer_PhoneNumber;
+    private String referredBy_clinic_or_doctor;
+    private String familyMembers_seen_here;
+    private String imageUrl;
+}
