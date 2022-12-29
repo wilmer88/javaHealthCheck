6a2f7155746d71e4e41a45384251004f17eba495
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-29 18:13:37 -0500

    created constructor for the person class with getter and setters for variables

--- src/main/java/com/wilmerbaby/healthcheck/model/Person.java
+++ src/main/java/com/wilmerbaby/healthcheck/model/Person.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 @Entity
 public class Person implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)//GenerationType can cause problems depending on the database used
+    @GeneratedValue(strategy = GenerationType.AUTO)//GenerationType can cause problems in performance and with database used
     @Column(nullable = false, updatable = false)//parameters on column annotation specify that ID will not allow to be updated
     private long personId;
     private String personFirstName;
@@ -18,14 +18,116 @@ public class Person implements Serializable {
     private int age;
     private String sex;
     private String personAddress;
-    private int ss_num;
-    private int home_phoneNumber;
-    private String cell_phoneNumber;
+    private int ssNum;
+    private int homePhoneNumber;
+    private String cellNumber;
     private String personEmail;
     private String occupation;
     private String employerName;
-    private int employer_PhoneNumber;
-    private String referredBy_clinic_or_doctor;
-    private String familyMembers_seen_here;
+    private int employerPhoneNumber;
+    private String referredBy;
+    private String familyMembersSeenHere;
     private String imageUrl;
+    public Person(){}
+    public Person(String personFirstName, String personLastName, String birthDay,int age, String sex, String personAddress, int ssNum, String cellNumber, String personEmail)
+    {
+        this.personFirstName = personFirstName;
+        this.personLastName = personLastName;
+        this.birthDay = birthDay;
+        this.age = age;
+        this.sex = sex;
+        this.personAddress = personAddress;
+        this.ssNum = ssNum;
+        this.cellNumber = cellNumber;
+        this.personEmail = personEmail;
+    }
+
+    public Long getPersonId(){
+        return personId;
+    }
+
+    public void setPersonId(long personId1){
+        this.personId = personId1;
+    }
+
+    public String getPersonName(){
+        return personFirstName +" "+ personLastName;
+    }
+
+    public void setPersonFirstName(String firstName){
+        this.personFirstName = firstName;
+    }
+
+    public void setPersonLastName(String lastName){
+        this.personLastName = lastName;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String dob){
+        this.birthDay = dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int Age) {
+        this.age = Age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex1){
+        this.sex = sex1;
+    }
+
+    public String getPersonAddress(){
+        return personAddress;
+    }
+
+    public void setPersonAddress(String address){
+        this.personAddress = address;
+    }
+
+    public int getSsNum() {
+        return ssNum;
+    }
+
+    public void setSsNum(int ssNum1){
+        this.ssNum = ssNum1;
+    }
+
+    public String getCellNumber(){
+        return cellNumber;
+    }
+
+    public void setCellNumber(String cellNum){
+        this.cellNumber = cellNum;
+    }
+
+    public String getPersonEmail(){
+        return personEmail;
+    }
+
+    public void setPersonEmail(String email){
+        this.personEmail= email;
+    }
+
+    @Override
+    public String toString(){
+        return "Patient{" +
+                "id= " + personId +
+                ", name='" + personFirstName + " " + personLastName + '\'' +
+                ", email='" + personEmail + '\'' +
+                ", phone='" + cellNumber + '\'' +
+                ", address='" + personAddress + '\'' +
+                '}';
+    }
+
+
 }
