56582c7e7a907632d36ce13a9068ee464731a165
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-30 02:55:23 -0500

    succesfully connected to data base. starting configuring hibernate orm

--- src/main/java/com/wilmerbaby/healthcheck/HealthCheckApplication.java
+++ src/main/java/com/wilmerbaby/healthcheck/HealthCheckApplication.java
@@ -1,10 +1,14 @@
 package com.wilmerbaby.healthcheck;
-
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.wilmerbaby.healthcheck.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class HealthCheckApplication implements CommandLineRunner {
@@ -18,6 +22,9 @@ public class HealthCheckApplication implements CommandLineRunner {
 	@Override
 	public void run(String...args) throws  Exception {
 		String sql = "select * from person";
+		List<Person> persons = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Person.class));
+
+		persons.forEach(System.out :: println);
 
 	}
 
