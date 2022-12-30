0b61d857457936d5d16a1c9cab4dce9b68b5174c
Wilmer Rivera <wilmerbaby@gmail.com>
2022-12-30 01:02:33 -0500

    downloaded oracle developr and orecal database. added dependencies to pom.xml with env variables started connection configuration to database with springboot and spring tool

---
+++ bin/README.md
@@ -0,0 +1,1 @@
+# javaHealthCheck
--- src/main/java/com/wilmerbaby/healthcheck/HealthCheckApplication.java
+++ src/main/java/com/wilmerbaby/healthcheck/HealthCheckApplication.java
@@ -1,13 +1,24 @@
 package com.wilmerbaby.healthcheck;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class HealthCheckApplication {
+public class HealthCheckApplication implements CommandLineRunner {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(HealthCheckApplication.class, args);
+	}
+	@Override
+	public void run(String...args) throws  Exception {
+		String sql = "select * from person";
+
 	}
 
 }
